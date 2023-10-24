d092c9d31385389ad3366452ec2e1af2c5e4af7c
turelyk-arsen <114808096+turelyk-arsen@users.noreply.github.com>
2023-10-24 22:40:00 +0200

    add jobParameters

--- src/main/java/example/billingjob/BillingJob.java
+++ src/main/java/example/billingjob/BillingJob.java
@@ -4,6 +4,7 @@ import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.repository.JobRepository;
 
 public class BillingJob implements Job {
@@ -19,16 +20,24 @@ public class BillingJob implements Job {
 
 	@Override
 	public void execute(JobExecution execution) {
-		try {
-			throw new Exception("Unable to process billing information");
-		} catch (Exception exception) {
-//		System.out.println("processing billing information");
-		execution.addFailureException(exception);
-		execution.setStatus(BatchStatus.COMPLETED);
-		execution.setExitStatus(ExitStatus.FAILED.addExitDescription(exception.getMessage()));
-		} finally {
-		this.jobRepository.update(execution);
-		}
+		JobParameters jobParameters = execution.getJobParameters();
+		String inputFile = jobParameters.getString("input.file");
+		System.out.println("processing billing information");
+	    execution.setStatus(BatchStatus.COMPLETED);
+	    execution.setExitStatus(ExitStatus.COMPLETED);
+	    this.jobRepository.update(execution);
+	    
+//		try {
+//			throw new Exception("Unable to process billing information");
+//		} catch (Exception exception) {
+////		System.out.println("processing billing information");
+//		execution.addFailureException(exception);
+//		execution.setStatus(BatchStatus.COMPLETED);
+//		execution.setExitStatus(ExitStatus.FAILED.addExitDescription(exception.getMessage()));
+//		} finally {
+//		this.jobRepository.update(execution);
+//		}
+		
 //	    System.out.println("processing billing information");
 //	    execution.setStatus(BatchStatus.COMPLETED);
 //	    execution.setExitStatus(ExitStatus.COMPLETED);
